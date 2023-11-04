46b6bf84bb1e859151c26bbfefa9aeb234e1a275
Braden Hirschi <bradenhirschi29@gmail.com>
2023-11-03 20:49:15 -0500

    Added common names

--- app/fast-rename-button.tsx
+++ app/fast-rename-button.tsx
@@ -2,6 +2,8 @@
 
 import { useEffect, useRef, useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
+import { MdOutlineBedroomChild, MdOutlineDining } from "react-icons/md";
+import { FaCouch, FaKitchenSet } from "react-icons/fa6";
 import Image from "next/image";
 
 const preselectedNames = [
@@ -70,7 +72,7 @@ const FastRenameButton = ({ images }: { images: any[] }) => {
       </button>
       {modalOpen && (
         <div className="fixed inset-0 h-screen w-screen bg-black/25 flex items-center justify-center">
-          <div className="bg-white max-w-md rounded-lg p-4 text-gray-500">
+          <div className="bg-white max-w-lg rounded-lg p-4 text-gray-500">
             <h3 className="float-left text-xl font-semibold">{`Image ${
               currentImageIndex + 1
             } of ${images.length} - ${images[currentImageIndex].title}`}</h3>
@@ -88,6 +90,44 @@ const FastRenameButton = ({ images }: { images: any[] }) => {
               src={images[currentImageIndex].url}
               alt={images[currentImageIndex].title}
             />
+
+            <div className="grid grid-cols-5 gap-2 mb-4">
+              <button
+                onClick={() => changeImageName("Kitchen")}
+                className="hover:bg-teal-400 hover:text-white rounded-md border border-gray-200 p-2 gap-1 flex flex-col items-center justify-center"
+              >
+                <FaKitchenSet size={22} />
+                <p className="text-xs">Kitchen</p>
+              </button>
+              <button
+                onClick={() => changeImageName("Bathroom")}
+                className="hover:bg-teal-400 hover:text-white rounded-md border border-gray-200 p-2 gap-1 flex flex-col items-center justify-center"
+              >
+                <FaKitchenSet size={22} />
+                <p className="text-xs">Bathroom</p>
+              </button>
+              <button
+                onClick={() => changeImageName("Living Room")}
+                className="hover:bg-teal-400 hover:text-white rounded-md border border-gray-200 p-2 gap-1 flex flex-col items-center justify-center"
+              >
+                <FaCouch size={22} />
+                <p className="text-xs">Living Room</p>
+              </button>
+              <button
+                onClick={() => changeImageName("Bedroom")}
+                className="hover:bg-teal-400 hover:text-white rounded-md border border-gray-200 p-2 gap-1 flex flex-col items-center justify-center"
+              >
+                <MdOutlineBedroomChild size={22} />
+                <p className="text-xs">Bedroom</p>
+              </button>
+              <button
+                onClick={() => changeImageName("Dining Room")}
+                className="hover:bg-teal-400 hover:text-white rounded-md border border-gray-200 p-2 gap-1 flex flex-col items-center justify-center"
+              >
+                <MdOutlineDining size={22} />
+                <p className="text-xs">Dining Room</p>
+              </button>
+            </div>
 
             <input
               placeholder="New image name"
